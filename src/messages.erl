f9f2678459ef84db17ada89dd18a1789fa035909
achan1989 <achan1989@gmail.com>
2017-04-30 21:16:51 +0100

    Update messages module after changes to message construction.

--- src/messages.erl
+++ src/messages.erl
@@ -2,21 +2,21 @@
 
 -export([password/1,nickname/1,user/2,user/3,quit/0,quit/1]).
 
+-define(USER_UNUSED_PARAM, "*").
+
 
 password(Password) ->
-    message:new(<<"PASS">>, [Password]).
+    message:new("PASS", [], Password).
 
 nickname(Nickname) ->
-    message:new(<<"NICK">>, [Nickname]).
+    message:new("NICK", [], Nickname).
 
 user(Username, Realname) ->
-    user(Username, <<"0">>, Realname).
+    user(Username, "0", Realname).
 user(Username, Mode, Realname) ->
-    message:new(<<"USER">>, [Username, Mode, <<"*">>, Realname]).
+    message:new("USER", [Username, Mode, ?USER_UNUSED_PARAM], Realname).
 
 quit() ->
-    quit(none).
-quit(none) ->
-    message:new(<<"QUIT">>, []);
+    quit("").
 quit(Reason) ->
-    message:new(<<"QUIT">>, [Reason]).
+    message:new("QUIT", [], Reason).
